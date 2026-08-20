94b65d49f34d261aa9db939337c6d7a1b91a1dfc
smile-core <smileshalom74@gmail.com>
2026-08-20 11:50:14 +0200

    connected backend and frontend

--- backend/README.md
+++ backend/README.md
@@ -1,10 +1,10 @@
-for docker
+for deployment
 
 sudo docker build -t fastapi-backend .
 sudo docker run -d -p 8000:8000 --name fastapi-backend fastapi-backend
 curl http://localhost:8000/
 
-for uv python
+for dev
 
 for .venv: uv sync
 uv run uvicorn main:app --reload
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
